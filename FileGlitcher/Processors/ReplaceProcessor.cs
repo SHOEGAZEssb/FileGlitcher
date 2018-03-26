b7b4a91daa3bf13b813e61992529dc2d13fd9529
Tim Stadler <timstadler905@googlemail.com>
2018-03-26 21:17:59 +0200

    Added ReplaceProcessor

---
+++ FileGlitcher/Processors/ReplaceProcessor.cs
@@ -0,0 +1,38 @@
+﻿using FileGlitcher.Processors.ByteProviders;
+using FileGlitcher.Processors.ByteRules;
+
+namespace FileGlitcher.Processors
+{
+  /// <summary>
+  /// Processor that replaces bytes.
+  /// </summary>
+  public class ReplaceProcessor : ProcessorBase
+  {
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="byteRule">Byte rule to apply.</param>
+    /// <param name="byteProvider">Provider of bytes.</param>
+    public ReplaceProcessor(ByteRuleBase byteRule, IByteProvider byteProvider)
+      : base(byteRule, byteProvider)
+    { }
+
+    /// <summary>
+    /// Applies this processor to the
+    /// given <paramref name="bytes"/>.
+    /// </summary>
+    /// <param name="bytes">Bytes to apply the processor to.</param>
+    /// <returns>Modified bytes.</returns>
+    public override byte[] Apply(byte[] bytes)
+    {
+      // todo: initialize byterule
+
+      while(_byteRule.NumBytesLeftToGlitch != 0)
+      {
+        bytes[_byteRule.GetNextByteIndex()] = ByteProvider.GetByte();
+      }
+
+      return bytes;
+    }
+  }
+}
---
+++ FileGlitcherTest/ProcessorTests/ReplaceProcessorTest.cs
@@ -0,0 +1,32 @@
+﻿using FileGlitcher.Processors;
+using FileGlitcher.Processors.ByteProviders;
+using FileGlitcher.Processors.ByteRules;
+using NUnit.Framework;
+
+namespace FileGlitcherTest.ProcessorTests
+{
+  /// <summary>
+  /// Tests for the <see cref="ReplaceProcessor"/>.
+  /// </summary>
+  [TestFixture]
+  class ReplaceProcessorTest
+  {
+    /// <summary>
+    /// Tests the <see cref="ReplaceProcessor.Apply(byte[])"/>.
+    /// </summary>
+    [Test]
+    public void ReplaceTest()
+    {
+      // given: ReplaceProcessor
+      byte[] bytes = new byte[] { 0, 1, 2, 3, 4 };
+      ReplaceProcessor proc = new ReplaceProcessor(new EveryNthByte(new ByteRange(0, (uint)bytes.Length), 1), new FixedByteProvider(5));
+
+      // when: applying the processor
+      var actual = proc.Apply(bytes);
+
+      // then: correctly modified
+      byte[] expected = new byte[] { 5, 5, 5, 5, 5 };
+      CollectionAssert.AreEqual(expected, actual);
+    }
+  }
+}
