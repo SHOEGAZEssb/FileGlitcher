72b18f0794ea11153449c4f3f4964a24d0b61485
Tim Stadler <timstadler905@googlemail.com>
2018-03-27 11:54:23 +0200

    Added BitShiftProcessor

---
+++ FileGlitcher/Processors/BitShiftProcessor.cs
@@ -0,0 +1,75 @@
+﻿using FileGlitcher.Processors.ByteIndexProviders;
+using FileGlitcher.Processors.ByteProviders;
+
+namespace FileGlitcher.Processors
+{
+  /// <summary>
+  /// Direction to shift the bits.
+  /// </summary>
+  public enum ShiftDirection
+  {
+    /// <summary>
+    /// Bits should be shifted right.
+    /// </summary>
+    Right,
+
+    /// <summary>
+    /// Bits should be shifted right.
+    /// </summary>
+    Left
+  }
+
+  /// <summary>
+  /// Processor that shifts bits.
+  /// </summary>
+  public class BitShiftProcessor : ByteProvidedProcessorBase
+  {
+    #region Properties
+
+    /// <summary>
+    /// Direction to shift the bits.
+    /// </summary>
+    public ShiftDirection Direction;
+
+    #endregion Properties
+
+    #region Construction
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="byteIndexProvider">Provider of byte indexes.</param>
+    /// <param name="byteProvider">Provider of byte values.</param>
+    /// <param name="direction">Direction to shift the bits.</param>
+    public BitShiftProcessor(ByteIndexProviderBase byteIndexProvider, IByteProvider byteProvider, ShiftDirection direction) 
+      : base(byteIndexProvider, byteProvider)
+    {
+      Direction = direction;
+    }
+
+    #endregion Construction
+
+    /// <summary>
+    /// Applies this processor to the
+    /// given <paramref name="bytes"/>.
+    /// </summary>
+    /// <param name="bytes">Bytes to apply the processor to.</param>
+    /// <returns>Modified bytes.</returns>
+    public override byte[] Apply(byte[] bytes)
+    {
+      // todo: byterule
+
+      while(_byteIndexProvider.ByteIndexPool.Count != 0)
+      {
+        uint byteIndex = _byteIndexProvider.GetNextByteIndex();
+        byte byteToShift = bytes[byteIndex];
+        if (Direction == ShiftDirection.Left)
+          bytes[byteIndex] = (byte)(byteToShift << ByteProvider.GetByte());
+        else
+          bytes[byteIndex] = (byte)(byteToShift >> ByteProvider.GetByte());
+      }
+
+      return bytes;
+    }
+  }
+}
---
+++ FileGlitcherTest/ProcessorTests/BitShiftProcessorTest.cs
@@ -0,0 +1,50 @@
+﻿using FileGlitcher.Processors;
+using FileGlitcher.Processors.ByteIndexProviders;
+using FileGlitcher.Processors.ByteProviders;
+using NUnit.Framework;
+
+namespace FileGlitcherTest.ProcessorTests
+{
+  /// <summary>
+  /// Tests for the <see cref="BitShiftProcessor"/>.
+  /// </summary>
+  [TestFixture]
+  class BitShiftProcessorTest
+  {
+    /// <summary>
+    /// Tests if shifting right works.
+    /// </summary>
+    [Test]
+    public void ShiftRightTest()
+    {
+      // given: BitShiftProcessor
+      byte[] bytes = new byte[] { 0, 1, 2, 3, 4 };
+      BitShiftProcessor proc = new BitShiftProcessor(new EveryNthByteIndexProvider(new ByteRange(0, (uint)bytes.Length), 1), new FixedByteProvider(1), ShiftDirection.Right);
+
+      // when: executing the processor
+      var actual = proc.Apply(bytes);
+
+      // then: correctly modified
+      byte[] expected = new byte[] { 0, 0, 1, 1, 2 };
+      CollectionAssert.AreEqual(expected, actual);
+    }
+
+    /// <summary>
+    /// Tests if shifting left works.
+    /// </summary>
+    [Test]
+    public void ShiftLeftTest()
+    {
+      // given: BitShiftProcessor
+      byte[] bytes = new byte[] { 0, 1, 2, 3, 4 };
+      BitShiftProcessor proc = new BitShiftProcessor(new EveryNthByteIndexProvider(new ByteRange(0, (uint)bytes.Length), 1), new FixedByteProvider(1), ShiftDirection.Left);
+
+      // when: executing the processor
+      var actual = proc.Apply(bytes);
+
+      // then: correctly modified
+      byte[] expected = new byte[] { 0, 2, 4, 6, 8 };
+      CollectionAssert.AreEqual(expected, actual);
+    }
+  }
+}
