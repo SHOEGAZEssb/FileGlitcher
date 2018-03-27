8c37a73375747a373f3a0d26b9e0e6aaac3a02e3
Tim Stadler <timstadler905@googlemail.com>
2018-03-28 00:25:06 +0200

    Added ByteProvider

---
+++ FileGlitcher/Processors/ByteProviders/ByteProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace FileGlitcher.Processors.ByteProviders
+{
+  /// <summary>
+  /// Provides a specific set of bytes.
+  /// </summary>
+  public class ByteProvider : IByteProvider
+  {
+    #region Properties
+
+    /// <summary>
+    /// The byte pool.
+    /// </summary>
+    public byte[] Bytes;
+
+    #endregion Properties
+
+    #region Member
+
+    /// <summary>
+    /// Last used byte index.
+    /// </summary>
+    private uint _lastByteIndex;
+
+    #endregion Member
+
+    #region Construction
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="bytesToProvide">Bytes to provide.</param>
+    public ByteProvider(byte[] bytesToProvide)
+    {
+      if (bytesToProvide == null)
+        throw new ArgumentNullException(nameof(bytesToProvide));
+      if (bytesToProvide.Length == 0)
+        throw new ArgumentException(string.Format("{0} can't be empty", nameof(bytesToProvide)));
+
+      Bytes = bytesToProvide;
+    }
+
+    #endregion Construction
+
+    /// <summary>
+    /// Gets the next byte.
+    /// </summary>
+    /// <returns>Byte.</returns>
+    public byte GetByte()
+    {
+      if (_lastByteIndex == Bytes.Length)
+        _lastByteIndex = 0;
+      return Bytes[_lastByteIndex++];
+    }
+  }
+}
---
+++ FileGlitcherTest/ByteProvidersTests/ByteProviderTest.cs
@@ -0,0 +1,58 @@
+﻿using FileGlitcher.Processors.ByteProviders;
+using NUnit.Framework;
+
+namespace FileGlitcherTest.ByteProvidersTests
+{
+  /// <summary>
+  /// Tests for the <see cref="ByteProvider"/>
+  /// </summary>
+  [TestFixture]
+  class ByteProviderTest
+  {
+    /// <summary>
+    /// Tests if the correct values are provided.
+    /// </summary>
+    [Test]
+    public void ProvideTest()
+    {
+      // given: ByteProvider with bytes
+      byte[] bytes = new byte[] { 0, 1, 2, 3, 4 };
+      ByteProvider provider = new ByteProvider(bytes);
+
+      // when: getting the bytes
+      byte[] actual = new byte[10];
+      for(int i = 0; i < actual.Length; i++)
+      {
+        actual[i] = provider.GetByte();
+      }
+
+      // then: correct bytes were provided.
+      byte[] expected = new byte[] { 0, 1, 2, 3, 4, 0, 1, 2, 3, 4 };
+      CollectionAssert.AreEqual(expected, actual);
+    }
+
+    /// <summary>
+    /// Tests that the correct exception
+    /// is thrown when no bytes are provided.
+    /// </summary>
+    [Test]
+    public void NoBytesToProvideTest()
+    {
+      // when: creating a ByteProvider with no bytes
+      // then: ArgumentNullException
+      Assert.That(() => new ByteProvider(null), Throws.ArgumentNullException);
+    }
+
+    /// <summary>
+    /// Tests that the correct exception
+    /// is thrown when empty bytes are provided.
+    /// </summary>
+    [Test]
+    public void EmptyBytesToProvideTest()
+    {
+      // when: creating a ByteProvider with no bytes
+      // then: ArgumentNullException
+      Assert.That(() => new ByteProvider(new byte[0]), Throws.ArgumentException);
+    }
+  }
+}
